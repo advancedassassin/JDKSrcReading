2c2c2053045d5c050b25d5a542962976a90d3364
ydx <9329381014@qq.com>
2020-10-26 22:52:49 +0800

    JDK-source

---
+++ jdk/src/main/java/Test.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Test {
+
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<String>();
+
+        strings.add("1");
+
+        strings.add("2");
+
+        strings.add("3");
+
+        System.out.println(strings);
+    }
+}
